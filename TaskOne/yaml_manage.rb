61683a4ecde8ef5379be35649b8bec64a25c1855
GoldenFlame <goldenflames@gmail.com>
2010-10-26 16:49:55 +0300

    ui and yaml managing added

---
+++ TaskOne/yaml_manage.rb
@@ -0,0 +1,16 @@
+require 'yaml'
+
+class YamlManage
+  def self.create_char(name, pass)
+    
+  end
+  
+  def self.save_char(char)
+    
+  end
+  
+  
+  def self.load_file(path)
+   
+  end
+end
