41fb4aff3ba31ff4dce53a86ed80a7ab1db4ac5d
GoldenFlame <goldenflames@gmail.com>
2010-10-28 00:13:09 +0300

    Many new implementations

--- TaskOne/ui.rb
+++ TaskOne/ui.rb
@@ -1,3 +1,4 @@
+require 'rbconfig'
 
 class Ui
   def initialize(world)
@@ -6,6 +7,7 @@ class Ui
   end
   
   def login
+    clear_console
     puts "1.Register"
     puts "2.Login"
     @world.login(read_ch-48)
@@ -15,7 +17,14 @@ class Ui
     require "Win32API"
     Win32API.new("crtdll", "_getch", [], "L").Call
   end
-  
+
+  def clear_console
+    if(Config::CONFIG['host_os'] =~ /mswin|mingw/)
+      #system('cls')
+    else
+      #system('clear')
+    end
+  end
   
   def reg
     registered = false
@@ -43,7 +52,7 @@ class Ui
       puts 'Enter your password'
       pass = gets
       char = @world.logs(name)
-      if(char.class == :hash)
+      if(char != nil)
         if(char[:password].chomp == pass.chomp)
           logged_in = true
           puts 'You successfully logged in.'
@@ -54,6 +63,157 @@ class Ui
       end
     end
     return char
+  end
+  
+  def show_stats(char)
+    clear_console
+    puts "Name: #{char[:name]}"
+    puts "Class: #{char[:class]}"
+    puts "HP: #{char[:hp]}/#{char[:max_hp]}"
+    puts "Mana: #{char[:mana]}/#{char[:max_mana]}"
+    puts "Base damage: #{char[:base_dmg_min]}/#{char[:base_dmg_max]}"
+    puts "Press any key to go back."
+    read_ch
+  end
+  
+  def show_inventory(char)
+    clear_console
+    sw = char[:backpack][:sword].length
+    bw = char[:backpack][:bow].length
+    st = char[:backpack][:staff].length
+    puts "Your inventory:"
+    if(sw != 0 || bw != 0 || st != 0)
+      puts "1. Swords(#{sw})" if(sw != 0)
+      puts "2. Bows(#{bw})" if(bw != 0)
+      puts "3. Staffs(#{st})" if(st != 0)
+      puts "4. Go back"
+    else 
+      puts "Your inventory is empty."
+    end
+    @world.show_inventory(char,read_ch-48)
+  end
+  
+  def item_menu_inventory(char, arg)
+    clear_console
+    char[:backpack][arg].each_with_index{|x,y| puts "#{y+1}. #{x}"}
+    c = -1
+    until (char[:backpack][arg].length-1>= c && c >= 0)
+      c = read_ch - 49
+    end
+    
+    item_view(char,arg, char[:backpack][arg][c])
+  end
+  
+  def item_view(char,arg, item_name)
+    clear_console
+    item = @world.find_item(arg, item_name)
+    puts "Name: #{item[:name]}"
+    puts "Class: #{item[:class]}"
+    puts "Level requirement: #{item[:level]}"
+    puts "Damage #{item[:damage_min]}/#{item[:damage_max]}"
+    puts "Press any key to go back."
+    read_ch
+  end
+  
+  def go_to_world(char)
+    puts "1. Go to arena."
+    puts "2. Go to the wilds."
+    puts "3. Go to the shop."
+    puts "4. Go to the inn."
+    puts "5. View your stats."
+    puts "6. View your inventory."
+    puts "7. Exit game"
+    @world.go_to_world(char,read_ch-48)
+  end
+  
+  def inn(char,name)
+    clear_console
+    puts "Welcome to #{name}"
+    puts "1.Rest and regain your strength."
+    puts "2.Leave."
+    @world.inn(char,read_ch-48)
+  end
+  
+  def shop(char,name)
+    clear_console
+    puts "Welcome to #{name}"
+    puts "1. Buy sword."
+    puts "2. Buy bow."
+    puts "3. Buy magic staff."
+    puts "4. Leave."
+    @world.shop(char,read_ch-48)
+  end
+  
+  def inspect(char,item)
+    clear_console
+    puts item[:name]
+    puts "Level requirement: #{item[:level]}"
+    puts "Damage #{item[:damage_min]}/#{item[:damage_max]}"
+    if(char[:gold] >= item[:price])
+      puts "1. Buy."
+    else 
+      puts "You do not have enough money."
+    end
+    puts "2. Go back."
+    @world.inspect(char,item,read_ch-48)
+  end
+  
+  def item(item_list)
+    clear_console
+    item_list.each_with_index{|x,y| puts "#{y+1}. Inspect #{x[:name]}"}
+    read_ch-48
+  end
+
+  def wilds(char,city)
+    clear_console
+    if(city[:fight_area].to_i !=0)
+      puts "Go to:"
+      (1..city[:fight_area].to_i).collect{|x| area = "fight_#{x.to_s}"; puts x.to_s+". "+city[area.to_sym]}
+      @world.wilds(char,read_ch,city)      
+    else
+      puts "There are no wild areas which you can visit now."
+    end
+  end
+  
+  def arena(char)
+    clear_console
+    puts "Welcome to the arena."
+    puts "1.Fight random monster."
+    puts "2.Go back."
+    @world.arena(char,read_ch-48)
+  end
+  
+  def go_to_area(char,area)
+   
+  end
+  
+  def battle_info(char, enemy)
+    puts "You are finghting #{enemy[:name]}"
+    puts "Your stats:"
+    puts "HP: #{char[:hp]}/#{char[:max_hp]}"
+    puts "Mana: #{char[:mana]}/#{char[:max_mana]}"
+    puts "Base damage: #{char[:base_dmg_min]}/#{char[:base_dmg_max]}"
+    puts "----"
+    puts "#{enemy[:name]} stats:"
+    puts "HP: #{enemy[:hp]}/#{enemy[:max_hp]}"
+    puts "Base damage: #{enemy[:base_dmg_min]}/#{enemy[:base_dmg_max]}"
+  end
+  
+  def fight_menu(char,enemy)
+    puts "1.Basic attack"
+    puts "2.Skill attack"
+    puts "3.Use item"
+    puts "4.Run away"
+    @world.fight_menu(char, enemy,read_ch-48)
+  end
+  
+  def choose_class(char)
+    clear_console
+    puts 'Choose your characters class'
+    puts '1. Warrior(Melee fighter)'
+    puts '2. Archer(Ranged fighter)'
+    puts '3. Mage(Magic fighter)'
+    @world.choose_class(char,read_ch-48)
   end
   
 end
--- TaskOne/world.rb
+++ TaskOne/world.rb
@@ -1,8 +1,9 @@
 require File.join(File.dirname(__FILE__), 'yaml_manage.rb')
 require File.join(File.dirname(__FILE__), 'ui.rb')
+require File.join(File.dirname(__FILE__), 'fight.rb')
 
 class World
-  @interface
+  attr_accessor :fight, :interface 
   @city
   @exitcmd
   def initialize 
@@ -11,23 +12,24 @@ class World
     cities_number = world[:cities]
     @city = (1..cities_number.to_i).collect{|x| YamlManage.load_file("data/city/#{world[x.to_s.to_sym]}.yml")}
     @interface = Ui.new(self)
+    @fight = Fight.new(@interface)
   end
   
   def exit
     @exitcmd = true
   end  
   
-  def get_interface
-    @interface
-  end
-  
   def main_menu
-    char = @interface.login()
+    char = nil
+    while(char == nil) do
+      char = @interface.login()
+    end
     res = nil
     while(true)
       if(char[:class] == 0)
         res = @interface.choose_class(char)
       else 
+        @interface.clear_console
         res = check_lvlup(char), @interface.go_to_world(char)
       end
       break if(@exitcmd) 
@@ -39,5 +41,222 @@ class World
     YamlManage.save_char(char)
   end
   
+  def check_lvlup(char)
+    case char[:exp]
+    when 50 then char[:level] = 2
+    when 100 then char[:level] = 3
+    when 200 then char[:level] = 4
+    when 400 then char[:level] = 5
+    when 800 then char[:level] = 6
+    when 1600 then char[:level] = 7
+    when 2400 then char[:level] = 8
+    when 4000 then char[:level] = 9
+    when 5800 then char[:level] = 10
+    when 8000 then char[:level] = 11
+    when 10500 then char[:level] = 12
+    when 15000 then char[:level] = 13
+    when 21000 then char[:level] = 14
+    when 30000 then char[:level] = 15
+    when 43000 then char[:level] = 16
+    when 58000 then char[:level] = 17
+    when 70000 then char[:level] = 18
+    when 95000 then char[:level] = 19
+    when 120000 then char[:level] = 20
+    end
+  end
+#---------  
+  def class_warrior(char)
+    char[:class] = 1
+    char[:base_dmg_min] = 20
+    char[:base_dmg_max] = 30
+    char[:max_hp] = 100
+    char[:hp] = 100
+    char[:max_mana] = 10
+    char[:mana] = 10
+    YamlManage.save_char(char)
+  end
+
+  def class_archer(char)
+    char[:class] = 2
+    char[:base_dmg_min] = 10
+    char[:base_dmg_max] = 50
+    char[:max_hp] = 75
+    char[:hp] = 75
+    char[:max_mana] = 30
+    char[:mana] = 30
+    YamlManage.save_char(char)
+  end
   
+  def class_mage(char)
+    char[:class] = 3
+    char[:base_dmg_min] = 10
+    char[:base_dmg_max] = 20
+    char[:max_hp] = 50
+    char[:hp] = 50
+    char[:max_mana] = 100
+    char[:mana] = 100
+    YamlManage.save_char(char)
+  end
+#---------    
+
+  def rest(char)
+    char[:hp] = char[:max_hp]
+    char[:mana] = char[:max_mana]
+  end
+
+#---------
+  def find_arena_monster(city)
+    if(city[:fight_area].to_i !=0)
+      area = "f#{Random.new.rand(1..city[:fight_area].to_i)}monsters"
+      if(city[area.to_sym].size != 0)
+        city[area.to_sym][Random.new.rand(0..city[area.to_sym].size-1)]
+      end
+    end
+  end   
+  
+#---------   
+  def login(c)
+    case c
+    when 1 then @interface.reg
+    when 2 then @interface.logs
+    end
+  end
+  
+  def reg(name,pass)
+    YamlManage.create_char(name,pass)
+  end
+  
+  def logs(name)
+    YamlManage.load_file("data/user/#{name.chomp}.yml") if(File.exist? "data/user/#{name.chomp}.yml")
+  end
+    
+  
+  def find_item(arg, item_name)
+    item_return = nil
+    items = Dir.glob("data/item/sword*").collect{|x| YamlManage.load_file(x)}
+    items.each do |x|
+      if(x[:name] == item_name)
+        item_return = x
+      end
+    end
+    return item_return
+  end
+  
+  def show_inventory(char, c)
+    case c
+    when 1 then @interface.item_menu_inventory(char, :sword) if(char[:backpack][:sword] != 0)
+    when 2 then @interface.item_menu_inventory(char, :bow) if(char[:backpack][:bow] != 0)
+    when 3 then @interface.item_menu_inventory(char, :staff) if(char[:backpack][:staff] != 0)
+    when 4 then @interface.go_to_world(char)
+    else show_inventory(char, @interface.read_ch-48)
+    end
+  end
+    
+  def go_to_world(char,c)
+    case c
+    when 1 then @interface.arena(char)
+    when 2 then @interface.wilds(char, @city[char[:current_city]])
+    when 3 then @interface.shop(char, @city[char[:current_city]][:shop])
+    when 4 then @interface.inn(char, @city[char[:current_city]][:inn])
+    when 5 then @interface.show_stats(char)
+    when 6 then @interface.show_inventory(char)
+    when 7 then exit
+    end
+  end
+  
+  def inn(char,c)
+    case c
+    when 1 then rest(char)
+    when 2 then @interface.go_to_world(char)
+    else inn(char, @interface.read_ch-48)
+    end
+  end
+  
+  def shop(char,c)
+    case c
+    when 1 then item(char, "sword")
+    when 2 then item(char, "bow")
+    when 3 then item(char, "staff")
+    when 4 then @interface.go_to_world(char)
+    else shop(char, @interface.read_ch-48)
+    end
+  end
+  
+  def inspect(char,item,c)
+    if(char[:gold]>=item[:price])
+      case c
+      when 1 then buy_item(char,item)
+      when 2 then go_back(char,item[:class])
+      else inspect(char, item, @interface.read_ch-48)
+      end
+    else
+      case c
+      when 2 then go_back(char,item[:class])
+      else inspect(char, item, @interface.read_ch-48)
+      end
+    end
+  end
+  
+  def buy_item(char, item)
+    char[:gold] -= item[:price]
+    if(char[:backpack][item[:class].to_sym].class != Array.new.class)
+      tmp = char[:backpack][item[:class].to_sym][0]
+      char[:backpack][item[:class].to_sym] = Array.new
+      char[:backpack][item[:class].to_sym][0] = tmp
+    end
+    char[:backpack][item[:class].to_sym].push(item[:name])
+    YamlManage.save_char(char)
+  end
+  
+  def go_back(char,menu)
+    if(menu == :sword)
+      @interface.swords(char)
+    elsif(menu == :bow)
+      @interface.bows(char)
+    elsif(menu == :staff)
+      @interface.staffs(char)
+    end
+  end
+  
+  def item(char,item)
+    item_list = Dir.glob("data/item/#{item}*").collect{|x| YamlManage.load_file(x)}
+    c = @interface.item(item_list)
+    until (item_list.length>= c && c >= 0)
+      c = @interface.read_ch-48
+    end
+    @interface.inspect(char, item_list[c-1])
+  end
+  
+  def wilds(char,c,city)
+    area = "fight_#{c.to_s}"
+    @interface.go_to_area(char,city[area.to_sym])
+  end
+  
+  def arena(char,c)
+    case c
+    when 1 then fight.fight(char, find_arena_monster(@city[char[:current_city]]))
+    when 2 then @interface.go_to_world(char)
+    else arena(char,@interface.read_ch-48)
+    end
+  end
+  
+  def fight_menu(char, enemy, c)
+    case c
+    when 1 then fight.attack(char, enemy)
+    when 2 then fight.skill(char, enemy)
+    when 3 then fight.use_item(char)
+    #when 52 then defeat = true
+    else fight_menu(char, enemy, @interface.read_ch-48)
+    end
+  end
+  
+  def choose_class(char,c)
+    case c
+    when 1 then class_warrior(char)
+    when 2 then class_archer(char)
+    when 3 then class_mage(char)
+    else choose_class(char, @interface.read_ch-48)
+    end
+  end
+ 
 end
