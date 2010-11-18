c4b3de9bc8b557b0be7423e7349aa0bf923a7774
GoldenFlame <goldenflames@gmail.com>
2010-11-18 19:03:36 +0200

    update

--- TaskOne/lib/monster.rb
+++ TaskOne/lib/monster.rb
@@ -1,3 +1,4 @@
 class Monster < Entity
-  attr_accessor :name, :level, :base_dmg_min, :base_dmg_max, :max_hp, :hp, :exp_bonus, :gold_max
+  attr_reader :name, :level, :base_dmg_min, :base_dmg_max, :max_hp, :exp_bonus, :gold_max
+  attr_accessor :hp
 end
